1b60dfa392ba5091b88121544b5dd73b11ff8b45
maxiwell <maxiwell@gmail.com>
2016-07-10 20:25:11 -0300

    Fix and rebase to go back the 'data' folder
    In the commit that was added the TLM2 (Liana' author), the 'data' folder was removed wrongly.

--- bin/check.sh
+++ bin/check.sh
@@ -3,6 +3,6 @@
 for I in *.x86.out ; do
 
   TMP=`echo $I | cut -d '.' -f '1 2'`
-  diff --brief --report-identical-files ${TMP}.${ARCH}.out  $I   # data/$TMP.data
+  diff --brief --report-identical-files ${TMP}.${ARCH}.out  data/$TMP.data
 
 done
